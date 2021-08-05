8a97540d4dccddfa1e060bf489ecfb8d43b7dd32
Devon Wolfkiel <devonwolf@tutanota.com>
2021-08-05 16:50:29 -0700

    Post route for studios

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,41 @@
 import request from 'supertest';
 import app from '../lib/app.js';
+import database from '../lib/utils/database';
 
 describe('demo routes', () => {
-  
-  it('is not a test', () => {
-    console.log('I am not a test');
+  beforeEach(() => {
+    return database.sync({ force: true });
   });
   
+  it('creates a studio', () => {
+    const newStudio = {
+      name: 'TA Studio',
+      city: 'Your Computer',
+      state: 'OR',
+      country: 'USA'
+    };
+
+    return request(app)
+      .post('/api/v1/studios')
+      .send(newStudio)
+      .then(response => {
+        expect(response.body).toEqual({
+          id: 1,
+          ...newStudio
+        });
+      });
+  });
+
+  it.skip('gets all studios', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(response => {
+        expect(response.body).toEqual([{ id: 1, name: 'studio' }]);
+      });
+  });
+
+  it.skip('gets a studio by id', () => {
+
+  });
+
 });
