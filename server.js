5d96aae366ba5065893dcf9da9c57f5a51ee61ca
Devon Wolfkiel <devonwolf@tutanota.com>
2021-08-05 15:41:41 -0700

    Set up basic sequelize instance, test the connection when the server starts

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import app from './lib/app.js';
+import { testConnection } from './lib/utils/database.js';
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+  testConnection();
 });
 
 process.on('exit', () => {
