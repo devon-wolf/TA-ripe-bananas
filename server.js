dec448b4b387ea24864396060087efd3cdf4e7ab
Devon Wolfkiel <devonwolf@tutanota.com>
2021-08-05 18:21:05 -0700

    Remove sequelize connection test from server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-console */
 import app from './lib/app.js';
-import { testConnection } from './lib/utils/database.js';
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   console.log(`Started on ${PORT}`);
-  testConnection();
 });
 
 process.on('exit', () => {
