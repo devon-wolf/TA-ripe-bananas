0623cf7f561de73b51cb17cb6128495c8c24966c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-08-05 17:12:03 -0700

    Complete get route, write test for get by id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,40 +2,63 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import database from '../lib/utils/database';
 
+const newStudio = {
+  name: 'TA Studio',
+  city: 'Your Computer',
+  state: 'OR',
+  country: 'USA'
+};
+
 describe('demo routes', () => {
   beforeEach(() => {
     return database.sync({ force: true });
   });
+
+  beforeEach(() => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send(newStudio);
+  });
   
   it('creates a studio', () => {
-    const newStudio = {
-      name: 'TA Studio',
-      city: 'Your Computer',
+    const newerStudio = {
+      name: 'Another Studio',
+      city: 'Another Computer',
       state: 'OR',
       country: 'USA'
     };
 
     return request(app)
       .post('/api/v1/studios')
-      .send(newStudio)
+      .send(newerStudio)
+      .then(response => {
+        expect(response.body).toEqual({
+          id: 2,
+          ...newerStudio
+        });
+      });
+  });
+
+  it('gets all studios', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(response => {
+        expect(response.body).toEqual([{
+          id: 1,
+          ...newStudio
+        }]);
+      });
+  });
+
+  it('gets a studio by id', () => {
+    return request(app)
+      .get('/api/v1/studios/1')
       .then(response => {
         expect(response.body).toEqual({
           id: 1,
           ...newStudio
         });
       });
-  });
-
-  it.skip('gets all studios', () => {
-    return request(app)
-      .get('/api/v1/studios')
-      .then(response => {
-        expect(response.body).toEqual([{ id: 1, name: 'studio' }]);
-      });
-  });
-
-  it.skip('gets a studio by id', () => {
-
   });
 
 });
